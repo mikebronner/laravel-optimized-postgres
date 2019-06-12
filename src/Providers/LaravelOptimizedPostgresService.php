6a675ffc60b66e97e6d9a0f6b9a3f89f37576c88
Mike Bronner <mike@genealabs.com>
2019-06-12 10:49:46 -0700

    Update LaravelOptimizedPostgresService.php

--- src/Providers/LaravelOptimizedPostgresService.php
+++ src/Providers/LaravelOptimizedPostgresService.php
@@ -16,10 +16,30 @@ class LaravelOptimizedPostgresService extends ServiceProvider
     {
         //TODO: figure out how to overwrite the already loaded schema alias.
         AliasLoader::getInstance()->alias('Schema', Schema::class);
+        $this->registerSchemaMacros();
     }
 
     public function provides() : array
     {
         return ['genealabs-laravel-optimized-postgres'];
+    }
+
+    protected function registerSchemaMacros()
+    {
+        Blueprint::macro('dropIndexIfExists', function(string $index): Fluent {
+            if ($this->hasIndex($index)) {
+                return $this->dropIndex($index);
+            }
+
+            return new Fluent();
+        });
+
+        Blueprint::macro('hasIndex', function(string $index): bool {
+            $conn = Schema::getConnection();
+            $dbSchemaManager = $conn->getDoctrineSchemaManager();
+            $doctrineTable = $dbSchemaManager->listTableDetails($this->getTable());
+
+            return $doctrineTable->hasIndex($index);
+        });
     }
 }
